6a3ed46d881efc4c8ab299140d3c8cb11671240a
unknown <riteshsahoo62@gmail.com>
2025-07-23 17:37:31 +0530

    First setup commit

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEM_E_DOT ='-e .'
+def get_requirements(file_path: str)->List[str]:
+    '''
+    This function will return the list of requirements
+    '''
+    requirements=[]
+
+    with open(file_path) as file_obj:
+        requirements= file_obj.readlines()
+        requirements = [req.replace('\n',"") for req in requirements]
+
+        if HYPEM_E_DOT in requirements:
+            requirements.remove(HYPEM_E_DOT)
+
+    return requirements
+
+
+setup(
+name='mlproject',
+version='0.0.1',
+author='ritesh',
+author_email='riteshsahoo62@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+)
